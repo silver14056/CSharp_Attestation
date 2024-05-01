d3ca88ebd62adebeeee1468666bab270784b2973
Serg Litov <Serg@litov.com>
2024-05-01 15:40:30 +0300

    Добавлено решение задачи 3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+
+void Main()
+{
+    int arraySize = ReadInt("Введите размер массива: ");
+    int[] array = GenerateArray(arraySize, 0, 99);
+    PrintArray(array);
+    RecursionPrintArray(array);
+    
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GenerateArray(int size, int minRange, int maxRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; ++i)
+    {
+        tempArray[i] = rand.Next(minRange, maxRange + 1);
+    }
+    return tempArray;
+}
+
+void RecursionPrintArray(int[] arrayForPrint)
+{
+    if (arrayForPrint.Length < 1) return;
+    RecursionPrintArray(arrayForPrint[1..]);
+    System.Console.Write(arrayForPrint[0] + " ");
+    
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arrayForPrint) + "]");
+}
+
+
+Main();
